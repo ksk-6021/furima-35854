d80ab58fd04ffb15ae9d90b43bbd336722219b28
ksk-6021 <sgkg2215462@gmail.com>
2021-06-03 17:46:33 +0900

    商品出品ページへの遷移・アクション・バリデーション・アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name,                 presence: true
+  validates :explain,              presence: true
+  validates :category_id,          presence: true
+  validates :condition_id,         presence: true
+  validates :shipping_charge_id,   presence: true
+  validates :shipping_area_id,     presence: true
+  validates :shipping_time_id,     presence: true
+  validates :price,                presence: true
 end
