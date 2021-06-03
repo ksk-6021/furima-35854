9c7d40f6b836bb5595c184c3625d6aba61dc376b
ksk-6021 <sgkg2215462@gmail.com>
2021-06-03 20:41:19 +0900

    ActiveHashを用いたカテゴリーの選択機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :shiping_time
   has_one_attached :image
 
   validates :name,                 presence: true
   validates :explain,              presence: true
-  validates :category_id,          presence: true
-  validates :condition_id,         presence: true
-  validates :shipping_charge_id,   presence: true
-  validates :shipping_area_id,     presence: true
-  validates :shipping_time_id,     presence: true
-  validates :price,                presence: true
+  validates :category_id,          numericality: { other_than: 1 }
+  validates :condition_id,         numericality: { other_than: 1 }
+  validates :shipping_charge_id,   numericality: { other_than: 1 }
+  validates :shipping_area_id,     numericality: { other_than: 1 }
+  validates :shipping_time_id,     numericality: { other_than: 1 }
+  validates :price,                numericality: { other_than: 1 }
 end
