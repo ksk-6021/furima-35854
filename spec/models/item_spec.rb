b898f7e8939c182fab7feb082ebacb0d0ed55c62
ksk-6021 <sgkg2215462@gmail.com>
2021-06-07 15:26:25 +0900

    商品出品機能のコード修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -91,6 +91,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
+      it 'ユーザーが存在しないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
